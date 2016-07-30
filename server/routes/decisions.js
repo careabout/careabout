1f09cd14bcdced72554d759726aceb3ad91a3b35
tony-luisi <t.luisi@gmail.com>
2016-07-30 14:19:34 +1200

    changed mock data

--- server/routes/decisions.js
+++ server/routes/decisions.js
@@ -3,7 +3,28 @@ var path = require('path')
 var router = express.Router()
 
 var mockData = [
-  { name: 'decision1'}
+  { id: 1,
+    title: "consultation 1",
+    description: "description 1",
+    organisation: [{ id: 1, externalId: 2, name: "org 1" }],
+    url: "http://fakeurl.com",
+    start: "01/01/2016",
+    end: "31/12/2016",
+    status: "current",
+    topic: ["topic"],
+    location: ["Wellington"]
+  },
+  { id: 2,
+    title: "consultation 2",
+    description: "description 2",
+    organisation: [{ id: 2, externalId: 3, name: "org 2" }],
+    url: "http://fakeurl1.com",
+    start: "01/01/2017",
+    end: "31/12/2017",
+    status: "current",
+    topic: ["topic2"],
+    location: ["Auckland"]
+  },
 ]
 
 router.get('/', function (req, res) {
