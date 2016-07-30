080edbda046e3e9ea2eef98cfadb22ae6b69a950
josh-jacobsen <joshjacobsen5@gmail.com>
2016-07-31 11:57:47 +1200

    added test for getDecision

--- tests/server/decisions-route.test.js
+++ tests/server/decisions-route.test.js
@@ -1,9 +1,16 @@
 const test = require('tape')
 const decisionStore = require('../../server/decisionStore')
 
-test('getDecisions returns an object with locations property', t => {
+test('buildViewModel returns an object with locations property', t => {
   const expected = true
   const actual = decisionStore.getDecisions().hasOwnProperty('locations')
   t.equal(actual, expected)
-  t.end() 
+  t.end()
+})
+
+test('getDecision returns an object with property _id', t => {
+  const expected = true
+  const actual = decisionStore.getDecision('').hasOwnProperty('_id')
+  t.equal(actual, expected)
+  t.end()
 })
