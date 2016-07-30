8760aef78d3e959dfe1ad8e486f67a5d09a3d556
Rich Churcher <rich.churcher@gmail.com>
2016-07-30 16:31:22 +1200

    Capture locations from topic

--- server/processors/consultation.js
+++ server/processors/consultation.js
@@ -1,4 +1,5 @@
 module.exports = {
+  captureLocation: captureLocation,
   process: process,
   removeQuery: removeQuery
 }
@@ -12,4 +13,14 @@ function removeQuery (rawObject) {
   return {
     consultations: rawObject.consultations
   }
+}
+
+//^Local government - ([a-z ]+)$
+function captureLocation (topicString) {
+  const re = /^Local government - ([a-z ]+)$/i
+  const match = re.exec(topicString)
+  if (match) {
+    return match[1]
+  }
+  return null
 }
--- tests/server/processors/consultation.test.js
+++ tests/server/processors/consultation.test.js
@@ -8,3 +8,19 @@ test('removeQuery removes query property', (t) => {
   t.deepEqual(actual, expected)
   t.end()
 })
+
+test('captureLocation gets location from topic string', (t) => {
+  var expected = 'Far North'
+  var topic = 'Local government - Far North'
+  var actual = consultation.captureLocation(topic)
+  t.equal(actual, expected)
+  t.end()
+})
+
+test('captureLocation does not get location if improper format', (t) => {
+  const expected = null
+  const topic = 'Far North - Local government'
+  const actual = consultation.captureLocation(topic)
+  t.equal(actual, expected)
+  t.end()
+})
