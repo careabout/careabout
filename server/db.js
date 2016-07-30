8dbd77eb6d072a04e26806152b5728cc894fdaef
Don Smith <locksmithdon@gmail.com>
2016-07-30 21:21:20 +1200

    Added GET route for subscriptions

--- server/db.js
+++ server/db.js
@@ -1,23 +1,36 @@
 var mongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
-module.exports = {
-  save
-}
+module.exports = { save, get }
 
 function save(id, topics, locations) {
   return new Promise ( (resolve, reject) => {
-    mongoClient.connect(process.env.SUBSCRIPTION_DB, function (err, db) {
+    mongoClient.connect(process.env.SUBSCRIPTION_DB, (err, db) => {
       db.collection('test').insertOne({
         "id": id,
         "topics": topics,
         "locations": locations
       }, (err, result) => {
+        db.close()
         if (err) {
           reject(err)
         } else {
-          db.close()
           resolve()
+        }
+      })
+    })
+  })
+}
+
+function get (id) {
+  return new Promise ((resolve, reject) => {
+    mongoClient.connect(process.env.SUBSCRIPTION_DB, (err, db) => {
+      db.collection('test').findOne({"id": id}, (err, doc) => {
+        db.close()
+        if (err) {
+          reject(err)
+        } else {
+          resolve(doc)
         }
       })
     })
