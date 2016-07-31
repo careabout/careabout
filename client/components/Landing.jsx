a2e092b743288c9d43c5b2f6cdff9dce534d9101
tony-luisi <t.luisi@gmail.com>
2016-07-31 14:30:35 +1200

    added stylings

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default (props) => {
   return (
-    <div className="container">
+    <div>
       <div className="jumbotron" className="tickerParent">
-        <div className="container">
+
+
           <div id="ticker" className="row">
+                    <div className="container">
             <div id="text">
               <h1 className="gray">Care about</h1>
             </div>
@@ -27,13 +29,22 @@ export default (props) => {
                             <h1 className="lightergray">contributing</h1></li>
                           </ul>
                         </div>
+
                       </div>
-                      <div className="row">
-                        <div className="col-lg-12">
-                          <p><a className="btn btn-primary btn-lg">Get Started &raquo;</a></p>
-                        </div>
+
+
                       </div>
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-lg-12">
+                            <p><a className="btn btn-primary btn-lg">Get Started &raquo;</a></p>
+                          </div>
+                        </div>                      
+                      </div>
+
                     </div>
+
+                  <div className="container">
                     <div className="row" id="homeDetails">
                       <div className="col-lg-12">
                         <p>Laws and plans are being created and changed all the time without the people of the nation even being aware.</p><p>There should be a way to be notified when something close to your heart is about to be decided. That is where our App comes in. Put in your topics of interest and when an issue around that topic comes up then you will be informed and then there is the opportunity to engage with government to mold the country and area that you live in.</p>
@@ -51,7 +62,9 @@ export default (props) => {
                         </footer>
                       </div>
                     </div>
-                  </div>
+
                 </div>
-  )
-}
+    </div>
+
+              )
+            }
