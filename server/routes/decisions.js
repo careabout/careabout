722d18bc1359fa5dabf267bc1694dc63c3c55ce1
Tony Luisi <t.luisi@gmail.com>
2016-07-31 14:58:26 +1200

    Revert "bug fix no 2"

--- server/routes/decisions.js
+++ server/routes/decisions.js
@@ -27,28 +27,7 @@ router.get('/', (req, res) => {
   decisionStore.buildViewModel()
     .then(vm => {
       res.json({
-        decisions: [{ _id: "1abc",
-      title: "consultation 1",
-      description: "Far North District Council is consulting on a draft Bylaw for Te Pou Haerenga Tai - Twin Coast Cycle Trail, which runs from the Hokianga Harbour to the Bay of Islands. There is no single regulatory instrument for the management of the use of the Trail, which is",
-      organisation: "org",
-      url: "http://fakeurl.com",
-      start: "01/01/2016",
-      end: "31/12/2016",
-      status: "current",
-      topic: ["topic"],
-      location: ["Wellington"]
-    },
-    { _id: "2cde",
-      title: "consultation 2",
-      description: "description 2",
-      organisation: "org",
-      url: "http://fakeurl1.com",
-      start: "01/01/2017",
-      end: "31/12/2017",
-      status: "current",
-      topic: ["topic2"],
-      location: ["Auckland"]
-    }],
+        decisions: vm,
         locations: ["Hutt", "Southland", "Wellington", "Far North", "Christchurch", "Invercargill", "Palmerston North", "Tasman", "Tauranga", "Ashburton", "Queenstown", "Gisborne", "Waikato", "Otago", "Hamilton"],
         topics: ["Conservation", "Support", "Land and property", "Biosecurity", "Fisheries", "Education", "Electricity", "Gambling", "Food safety and biosecurity", "Tax", "Transport", "Business", "Food safety", "Environment", "Health", "Customs and excise"]
       })
